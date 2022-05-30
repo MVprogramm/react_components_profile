053c827f8555adbe06eb8cc6cc1283617283ab91
michael valiahmetov <michael.valiahmetov@gmail.com>
2022-05-30 15:48:35 +0300

    name without dot

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,7 +6,7 @@ function Profile(props) {
   return (
     <div className="profile">
       <div className="profile__name">
-        {props.userData.firstName} {props.userData.lastName}.
+        {props.userData.firstName} {props.userData.lastName}
       </div>
       <div className="profile__birth">
         Was born {moment(props.userData.birthDate).format("DD MMM YY")} in{" "}
